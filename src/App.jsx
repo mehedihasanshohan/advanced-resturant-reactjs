8d95e9eeead0123f7797b2c39db2c735f90aece0
mehedi hasan <mhshohan04@gmail.com>
2025-01-28 10:48:18 +0600

    project setup with tailwind 4, react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <h2 className='text-red-500 font-bold underline'>sohan</h2>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <h1 className="text-rose-50 font-bold">Welcome to the Home Page</h1>;
+};
+export default Home;
